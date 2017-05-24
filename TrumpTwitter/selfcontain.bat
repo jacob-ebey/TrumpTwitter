0e7c7f3ab7d098d2fefe565156c875dc43cdabd9
Jacob Ebey <jacob.ebey@live.com>
2017-05-24 16:42:25 -0700

    Removed unecessary file.

--- TrumpTwitter/selfcontain.bat
+++
@@ -1,4 +0,0 @@
-rmdir selfcontained
-mkdir selfcontained
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /lib:"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /out:selfcontained\ConsoleApp1.exe /target:winexe ConsoleApp1.exe Autofac.dll Microsoft.Threading.Tasks.dll Microsoft.Threading.Tasks.Extensions.Desktop.dll Microsoft.Threading.Tasks.Extensions.dll Newtonsoft.Json.dll System.Net.Http.Extensions.dll System.Net.Http.Primitives.dll Tweetinvi.dll
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /lib:"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /out:selfcontained\ConsoleApp1.exe /target:exe ConsoleApp1.exe Autofac.dll Microsoft.Threading.Tasks.dll Microsoft.Threading.Tasks.Extensions.Desktop.dll Microsoft.Threading.Tasks.Extensions.dll Newtonsoft.Json.dll System.Net.Http.Extensions.dll System.Net.Http.Primitives.dll Tweetinvi.dll
